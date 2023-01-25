797b246f73b2a5848dac14ba8a508373cebec85f
coo1isj <jeremypiperni@hotmail.com>
2023-01-25 00:39:45 -0500

    Updated java files

--- 1-Lexical Analyzer/src/lexdriver.java
+++ 1-Lexical Analyzer/src/lexdriver.java
@@ -1,27 +1,19 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.ArrayList;
 
 public class lexdriver {
-
+	
 	public static void main(String[] args) {
 		
-		String file = "Testing Source Files/lexpositivegrading.src";
-		BufferedReader reader;
-		try {
-			reader = new BufferedReader(new FileReader(file));
-			//String currentLine = reader.readLine();
-			//System.out.println(currentLine);
-			Token token = lexicalanalyzer.nextToken(reader);
-			System.out.println(token);
-			reader.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    
+		String file = "Testing Source Files/testing.src";
+		lexicalanalyzer lexAnalyzer = new lexicalanalyzer(file);
+		ArrayList<Token> tokenList = new ArrayList<Token>();
 		
-
+		Token token;
+		do {
+			token = lexAnalyzer.nextToken();
+			tokenList.add(token);
+		} while (token != null);
+		
 	}
 
 }
--- 1-Lexical Analyzer/src/lexicalanalyzer.java
+++ 1-Lexical Analyzer/src/lexicalanalyzer.java
@@ -1,21 +1,64 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class lexicalanalyzer {
 
-	public static Token nextToken(BufferedReader reader) {
+	private BufferedReader reader;
+	
+	public lexicalanalyzer(String file) {
+		try {
+			reader = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Token nextToken() {
 		int state = 1;
 		Token token = null;
-		//do {
+		do {
+			
 			try {
-				char c = (char) reader.read();
-				token = new Token(Character.toString(c),Character.toString(c),1);
+				reader.mark(100);
+				boolean isDone = checkForEnd();
+				if (isDone) {
+					return token;
+				}
+				reader.reset();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		//} while(true);
+			
+			char lookup = nextChar();
+			token = new Token(Character.toString(lookup),Character.toString(lookup),1);
+			System.out.println(token);
+		} while (token != null);
+			
 		return token;
+	}
+	
+	public char nextChar() {
+		char c = 0;
+		try {
+			c = (char) reader.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return c;
+	}
+	
+	public boolean checkForEnd() {	
+		try {
+			int c = 0;
+			if ((c = reader.read()) == -1) {
+				return true;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 	
 }
