f74ae36b5dcdf97f53a0263ae5bbec7a5af7c7fd
coo1isj <jeremypiperni@hotmail.com>
2023-01-26 19:43:35 -0500

    Added implementation for backtracking operators, for multi-character operators. Updated newline and end of file implementation. Updated dfa

--- 1-Lexical Analyzer/src/lexicalanalyzer.java
+++ 1-Lexical Analyzer/src/lexicalanalyzer.java
@@ -9,6 +9,7 @@ public class lexicalanalyzer {
 	private BufferedReader reader;
 	private MyTableModel table = new MyTableModel();
 	private static int line = 1;
+	private static int lineCounter = 0;
 	
 	public lexicalanalyzer(String file) {
 		try {
@@ -23,49 +24,85 @@ public class lexicalanalyzer {
 		Token token = null;
 		String lexeme = "";
 		do {
-			
-			// Check for end of file and new line
-			try {
-				reader.mark(1);
-				boolean isDone = checkForEnd();
-				if (isDone) {
-					return token;
-				}
-				reader.reset();
-				
-				boolean isNewLine = checkForNewLine();
-				if (isNewLine) {
-					line = line + 1;
-				} else {
-					reader.reset();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-			char lookup = nextChar();
 			int columnNumber = 0;
-			
-			if (Character.isWhitespace(lookup)) {
-				columnNumber = table.findColumn("whitespace");
+			char lookup = 0;
+			try {
+				// check for end of file
+				reader.mark(2);
+				if (checkForEnd()) {
+					columnNumber = table.findColumn("endoffile");
+					reader.reset();
+					
+				// check for new line
+				} else if (checkForNewLine()) {
+					columnNumber = table.findColumn("newline");
+					lineCounter = 1;
+					reader.mark(1);
+					lookup = ' ';
 				} else {
-				columnNumber = table.findColumn(Character.toString(lookup));
-				lexeme = lexeme + lookup;
-				if (columnNumber == -1) {
-					token = new Token("Error","Error",line);
-					System.out.println(token);
-					return token;
+					reader.mark(1);
+					lookup = nextChar();
+					
+					// check for whitespace
+					if (Character.isWhitespace(lookup)) {
+						columnNumber = table.findColumn("whitespace");
+						} else {
+						columnNumber = table.findColumn(Character.toString(lookup));
+						if (columnNumber == -1) {
+							token = new Token("Error","Error",line);
+							System.out.println(token);
+							return token;
+						}
+					}
 				}
+			} catch (IOException e1) {
+				e1.printStackTrace();
 			}
 			
+			// find next state of dfa
 			state = (int) table.getValueAt(state, columnNumber);
-			
+
+			// check to see if state is final state
 			Object finalValue = table.getValueAt(state, table.findColumn("final"));
 			if (Objects.nonNull(finalValue)) {
+				
+				// if at end of file, return null
+				if (finalValue.toString().equals("end")) {
+					return null;
+				}
+				
+				// update lexeme if not white space
+				if (!Character.isWhitespace(lookup)) {
+					lexeme = lexeme + lookup;
+				}
+				
+				// check to see if we must backtrack, delete last character of lexeme
+				Object backtrack = table.getValueAt(state, table.findColumn("backtrack"));
+				if (Objects.nonNull(backtrack)) {
+					if (!Character.isWhitespace(lookup)) {
+						lexeme = (lexeme.substring(0, lexeme.length() - 1));
+					}
+					try {
+						reader.reset();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				// returns token
 				String tokenName = (String) finalValue;
-				token = new Token(tokenName,lexeme,line);
+				token = new Token(tokenName,lexeme,line);				
 				System.out.println(token);
+			} else {
+				
+				// update lexeme when not in final state
+				if (!Character.isWhitespace(lookup)) {
+					lexeme = lexeme + lookup;
+				}
 			}
+			line = line + lineCounter;
+			lineCounter = 0;
+			
 		} while (token == null);
 			
 		return token;
@@ -87,6 +124,11 @@ public class lexicalanalyzer {
 			if ((c = reader.read()) == 10) {
 				return true;
 			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			reader.reset();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
